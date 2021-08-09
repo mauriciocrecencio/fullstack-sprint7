4d16c886082a4e3f2558f152dfecf3a5834c06c0
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2021-08-09 09:38:38 -0300

    add uma funcao para buscar todas as Categories pela Position

--- src/main/java/br/com/rchlo/store/repository/CategoryRepository.java
+++ src/main/java/br/com/rchlo/store/repository/CategoryRepository.java
@@ -1,8 +1,10 @@
 package br.com.rchlo.store.repository;
 
 import br.com.rchlo.store.domain.Category;
-import br.com.rchlo.store.domain.Payment;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+  List<Category> findAllByOrderByPosition();
 }
